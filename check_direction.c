e03ce34602833f730e771b4c91401a5ab5021fc5
Fredrik Larsson <89911893+fistbumpfromdestiny@users.noreply.github.com>
2021-11-13 09:41:21 +0100

    Add files via upload

--- check_direction.c
+++ check_direction.c
@@ -4,10 +4,10 @@ int check_direction(int** breadboard, int bb_cols, int row, int col) {
     /* If we're not in the outer edges of the breadboard, look for matching 
        resistor legs in either direction and return the direction to calling
        function. */
-    if ((col > 0) && (breadboard[row][col] == breadboard[row][col-1]\
+    if ((col > 0) && (breadboard[row][col] == breadboard[row][col-1]
         || breadboard[row][col-1] == -1)) 
         return LEFT; 
-    else if((col < bb_cols) && (breadboard[row][col] == breadboard[row][col+1]\
+    else if((col < bb_cols) && (breadboard[row][col] == breadboard[row][col+1]
         || breadboard[row][col+1] == -1))
         return RIGHT; 
     return 0;
